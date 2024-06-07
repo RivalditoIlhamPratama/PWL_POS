002861be9d8c0c12646280b6e5dde1a3a56e33c3
RivalditoIlhamPratama <rivaldodito100@gmail.com>
2024-06-07 19:22:50 +0700

    Jobsheet 9

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ManagerController;
+use App\Http\Controllers\AdminController;
 use App\Models\UserModel;
 use Illuminate\Support\Facades\Route;
 
@@ -49,6 +52,22 @@ Route::group(['prefix' => 'user'], function () {
     Route::get('/{id}/edit', [UserController::class, 'edit']);
     Route::put('/{id}', [UserController::class, 'update']);
     Route::delete('/{id}', [UserController::class, 'destroy']);
+
+//Jobsheet 9
+Route::get('/login', [AuthController::class, 'index'])->name('login');
+Route::post('/proses_login', [AuthController::class, 'proses_login'])->name('proses_login');
+Route::get('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/proses_register', [AuthController::class, 'proses_register'])->name('proses_register');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::group(['middleware' => ['cek_login:1']], function () {
+        Route::resource('admin', AdminController::class);
+    });
+    Route::group(['middleware' => ['cek_login:2']], function () {
+        Route::resource('manager', ManagerController::class);
+    });
+});
     
 });
 
